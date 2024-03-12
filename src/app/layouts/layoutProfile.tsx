da5445164c01507039938fe08b3ef36697c977b5
dungtm <truongmanhdung04@gmail.com>
2024-03-13 00:14:51 +0700

    config profile layout

---
+++ src/app/layouts/layoutProfile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../assets/styles/layout.css'
+const LayoutProfile = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <div className="backgroup-layout">
+    <main className="container">{children}</main>
+  </div>;
+};
+
+export default LayoutProfile;

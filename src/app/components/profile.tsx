bf7a0244c60eaa77cabe1edc51485419252dc554
truongmanhdung <71491379+truongmanhdung@users.noreply.github.com>
2024-03-13 17:21:57 +0700

    update code show image

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
-type Props = {}
-
+import React from "react";
+import { Row, Col } from "antd";
+type Props = {};
+import Image from "next/image";
+import ImageProfile from "../assets/images/avatar.jpg";
 const Profile = (props: Props) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="grid">
+      <Row>
+        <Col xs={24} sm={12} md={6} lg={6} xl={6}>
+          <Image
+            height={300}
+            width={300}
+            src={ImageProfile}
+            className="rounded-full"
+            alt="Avatar"
+          />
+        </Col>
+        <Col xs={24} sm={12} md={18} lg={18} xl={18}></Col>
+      </Row>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
